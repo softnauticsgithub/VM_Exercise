0d38dd1b8965d046a4b755234f8fa0c24b2ce5ff
jk03032024 <krupals251997@gmail.com>
2024-08-07 16:33:17 +0530

    test

---
+++ test_sh.sh
@@ -0,0 +1,43 @@
+deploymentYaml=$(cat <<EOF
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+ name: flask-app
+spec:
+ replicas: 1
+ selector:
+   matchLabels:
+	 app: flask-app
+ template:
+   metadata:
+	 labels:
+	   app: flask-app
+   spec:
+	 containers:
+	 - name: flask-app
+	   image: $1/$2:$3
+	   ports:
+	   - containerPort: 5000
+---
+apiVersion: v1
+kind: Service
+metadata:
+ name: flask-app-service
+spec:
+ type: LoadBalancer
+ selector:
+   app: flask-app
+ ports:
+   - port: 80
+	 targetPort: 8001
+EOF
+)
+
+if [ -e deployment.yaml ]
+then
+        rm -rf deployment.yaml
+fi
+
+echo "$deploymentYaml" > deployment.yaml
+kubectl apply -f deployment.yaml
+
