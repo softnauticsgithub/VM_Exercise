3fdb86a89659f53ee7b22ff3f62e89aaedeeaef4
jk03032024 <krupals251997@gmail.com>
2024-08-07 16:08:04 +0530

    test 2

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello():
-    return "Hello, World!"
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
-
+helloworld = Flask(__name__)
+@helloworld.route("/")
+def run():
+    return "{\"message\":\"Hello World Python v1\"}"
+if __name__ == "__main__":
+    helloworld.run(host="0.0.0.0", port=int("5000"), debug=True)
